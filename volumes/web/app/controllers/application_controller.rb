95c09a2b4c072cc7adbd167462ca635fdb48e64f
kuroweb <kuromitsu0104@gmail.com>
2023-10-02 00:37:38 +0900

    feat: キャッシュ有効化設定を追加

--- volumes/web/app/controllers/application_controller.rb
+++ volumes/web/app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  DEFAULT_AGE = 10.minutes
+
   before_action :no_cache
 
   def no_cache
@@ -6,5 +8,11 @@ class ApplicationController < ActionController::Base
       'private, no-store, no-cache, must-revalidate, post-check=0, pre-check=0'
     response.headers['Pragma'] = 'no-cache'
     response.headers['Expires'] = 'Thu, 19 Nov 1981 08:52:00 GMT'
+  end
+
+  def enable_cache
+    minutes = DEFAULT_AGE
+    response.headers['Cache-Control'] = "public, max-age=#{minutes}"
+    response.headers['Expires'] = minutes.from_now.httpdate
   end
 end
--- volumes/web/app/controllers/posts_controller.rb
+++ volumes/web/app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :enable_cache
+
   def index
     @posts = Post.all
   end
