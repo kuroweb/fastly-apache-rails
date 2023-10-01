adf5abcb46dba0ee75ed168358315ce1e9688f3f
kuroweb <kuromitsu0104@gmail.com>
2023-10-02 00:28:32 +0900

    feat: no_cache設定をrailsに追加

--- volumes/web/app/controllers/application_controller.rb
+++ volumes/web/app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :no_cache
+
+  def no_cache
+    response.headers['Cache-Control'] =
+      'private, no-store, no-cache, must-revalidate, post-check=0, pre-check=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = 'Thu, 19 Nov 1981 08:52:00 GMT'
+  end
 end
