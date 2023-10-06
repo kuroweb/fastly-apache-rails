25f0f17ff159f876e1c356ec9790e75debd00120
kuroweb <kuromitsu0104@gmail.com>
2023-10-06 17:31:22 +0900

    feat: lintする

--- volumes/web/app/controllers/application_controller.rb
+++ volumes/web/app/controllers/application_controller.rb
@@ -4,15 +4,15 @@ class ApplicationController < ActionController::Base
   before_action :no_cache
 
   def no_cache
-    response.headers['Cache-Control'] =
-      'private, no-store, no-cache, must-revalidate, post-check=0, pre-check=0'
-    response.headers['Pragma'] = 'no-cache'
-    response.headers['Expires'] = 'Thu, 19 Nov 1981 08:52:00 GMT'
+    response.headers["Cache-Control"] =
+      "private, no-store, no-cache, must-revalidate, post-check=0, pre-check=0"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Thu, 19 Nov 1981 08:52:00 GMT"
   end
 
   def enable_cache
     minutes = DEFAULT_AGE
-    response.headers['Cache-Control'] = "public, max-age=#{minutes}"
-    response.headers['Expires'] = minutes.from_now.httpdate
+    response.headers["Cache-Control"] = "public, max-age=#{minutes}"
+    response.headers["Expires"] = minutes.from_now.httpdate
   end
 end
